83c61db30ec53a2c85888e48025243e578bfb6d3
Vjeko Babic <babic@appropo.net>
2015-08-14 17:17:06 +0200

    Filters updated

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ angular.module('firstPage', [])
 
 	$httpProvider.interceptors.push(function(){
 
-		return {
+		return {  // Returning response from HTTP request
 
 			response: function(req) {
 
@@ -19,16 +19,18 @@ angular.module('firstPage', [])
 
 })
 
-.service('myService', function ($http){
+.service('myService', function ($http){  
 
 	var credentials = {
+
         username: 'admin', 
         password: 'admin',
         grant_type: 'password',
         client_id: 'ifair-app'
+        
         };
 
-this.getToken = function(){
+this.getToken = function(){ // Getting a token from a server.
 
 	console.log("Getting a TOKEN...");
 
@@ -37,36 +39,36 @@ this.getToken = function(){
         .success(function (data, status, headers, config) {
        
             sessionStorage.setItem('token', data.access_token);
+
             console.log("Token successfully fetch!");
+
         })
         .error(function (data, status, headers, config) {
-            console.error("in error");
+
+            console.error("Error during token fetching : " + headers);
+
         });
-
-
 
 }
 
-	this.getData = function (){
+	this.getData = function (){ // Initial server call collecting tom articles.
  
-	return $http({
+		return $http({
 
-				maethod: 'POST', 
-				url: 'http://grandvision-ifair-server.appropo.info/api/top-articles',
-				headers: {'AUTHORIZATION' : 'Bearer ' + sessionStorage.getItem('token')} 
+			maethod: 'POST', 
+			url: 'http://grandvision-ifair-server.appropo.info/api/top-articles',
+			headers: {'AUTHORIZATION' : 'Bearer ' + sessionStorage.getItem('token')} 
 
-			})
+		})
+		.success(function(data){
 
-			.success(function(data){
+			console.log("Successfully Fetch ALL data!");
 
-				console.log("Successfully Fetch ALL data!");
+		});
 
-			});
+	}
 
-		}
-
-
-	this.getModelData = function (model){
+	this.getModelData = function (model){ // Collecting data for particular model.
  
 	return $http({
 
@@ -84,13 +86,12 @@ this.getToken = function(){
 
 		}
 
-
-	this.getFilterData = function (filter, value){
+	this.getFilterData = function (filters){ // Filtering function!
  
 	return $http({
 
 				maethod: 'GET', 
-				url: 'http://grandvision-ifair-server.appropo.info/api/top-articles?' + filter + '=' + value,
+				url: 'http://grandvision-ifair-server.appropo.info/api/top-articles?' + filters,
 				headers: {'AUTHORIZATION' : 'Bearer ' + sessionStorage.getItem('token')} 
 
 			})
@@ -108,7 +109,9 @@ this.getToken = function(){
 .controller('displayModels', ['$scope', 'myService', function ($scope, myService) { 
 
 
-		$scope.visible = false ;
+		$scope.visible = false ; // Visibility status init!
+		var filters = ""; // Variable for storing filter status.
+		var lastFiltered; // Storing data from the last filter for the back function.
 		
 		
 		console.log("Starting APP !");
@@ -117,18 +120,17 @@ this.getToken = function(){
 
 		myService.getData().success(function(data){
 
-
-		$scope.mainData = data._embedded.top_articles;
-		$scope.allData = data;
-		console.log(data);
+			$scope.mainData = data._embedded.top_articles;
+			$scope.allData = data;
+			console.log(data);
 
 		});
-
 
 		$scope.getModel = function(model) {
 
 			console.log(model);
-			$scope.visible = true;
+
+			$scope.visible = true; // Changing visibility !
 
 			myService.getModelData(model).success(function(data){
 
@@ -141,17 +143,42 @@ this.getToken = function(){
 
 		}
 
-
 		$scope.filterModels = function (filter, value){
 
-			myService.getFilterData(filter, value).success(function(data){
+			
+			if (filters != "") {
+
+				filters += "&";
+				lastFiltered = "&";
+
+			}
+
+			filters += filter + "=" + value
+			lastFiltered += filter + "=" + value
+
+			myService.getFilterData(filters).success(function(data){
 
 				$scope.mainData = data._embedded.top_articles;
-				//$scope.allData = data;
+				$scope.allData = data;
 
-			})
+				console.log(data);
+
+			});
 
 		}
 
+		$scope.back = function(){
 
-	}]);
+			filters = filters.substring(0, filters.length - lastFiltered.length);
+			console.log(filters);
+
+			myService.getFilterData(filters).success(function(data){
+
+				$scope.mainData = data._embedded.top_articles;
+				$scope.allData = data;
+
+			});
+
+		}
+
+}]);
